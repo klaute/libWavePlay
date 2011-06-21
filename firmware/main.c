074f2980b6519c1386e53d91d2782eccf97ef534
Kai Lauterbach <klaute@web.de>
2011-06-21 15:58:37 +0200

    First implementation for testing.

--- firmware/main.c
+++ firmware/main.c
@@ -1,5 +1,64 @@
 
 #include "main.h"
+#include "wavedata.c"
+
+int main(void) {
+
+    init();
+
+    _delay_ms(0xff);
+    lcd_clearDisplay();
+
+    _delay_ms(0xff);
+    lcd_gotoxy(0,0);
+    lcd_putstr(soff);
+
+    //uint16_t cnt = 0;
+
+    do {
+
+        if ( mm_btnchk(PC0) ) {
+
+            lcd_gotoxy(0,0);
+            lcd_putstr(son);
+
+            timerCtrl(1);
+            timerCtrl(4);
+
+        } else if ( mm_btnchk(PC1) ) {
+
+            lcd_gotoxy(0,0);
+            lcd_putstr(soff);
+            timerCtrl(0);
+
+            timerCtrl(3);
+        }
+
+        /* Lesetest des wav im progmem. * /
+        uint8_t val = pgm_read_byte(&data_wav[cnt]);
+
+        char sval[10];
+        lcd_gotoxy(0,1);
+        utoa( val, sval, 16 );
+        lcd_putstr(sval);
+        lcd_gotoxy(5,1);
+        utoa( cnt, sval, 10 );
+        lcd_putstr(sval);
+
+        cnt++;
+        if (cnt >= data_wav_len)
+            cnt = 0;
+        */
+
+        _delay_ms(25);
+
+        //mm_LEDCode(2); // toggle status led
+
+    } while (1==1);
+
+    return 0;
+
+}
 
 void init(void)
 {
@@ -15,7 +74,8 @@ void init(void)
 
     // Initialisieren von Pin5 an PortB als Ausgang,
     // zum ansteuern des Summers.
-    DDRB &= ~( 1 << PB5 );
+    DDRB &= ~( 1 << PB5 ); // OC0B
+    DDRB &= ~( 1 << PB6 ); // OC0A
 
     // LED einschalten
     PORTB |= ( 1 << PB2 ); 
@@ -32,23 +92,26 @@ void init(void)
     PORTC |= ( 1 << PC1 );
     PORTC |= ( 1 << PC0 );
 
-    // Timer1 initialisieren
-    TCCR1A = 0x00;
-    TCCR1B = 0x00;
+    // Timer0 initialisieren
+    TCCR0A = 0x00;
+    TCCR0B = 0x00;
  
-    // Die PullUp-Widerst�nde einschalten.
-    PORTC |= ( 1 << PC3 );
-    PORTC |= ( 1 << PC2 );
-    PORTC |= ( 1 << PC1 );
-    PORTC |= ( 1 << PC0 );
-
-    // Es soll eine Interrupt ausgel�st werden wenn ein Overflow auftritt oder
+    // Timer2 initialisieren
+    TCCR2A = 0x00;
+    TCCR2B = 0x00;
+ 
+    // Es soll eine Interrupt ausgeloest werden wenn ein Overflow auftritt oder
     // der Vergleichswert erreicht wurde.
-    TIMSK1 = (1 << OCIE1A);
+    TIMSK0 = 0x00; // Normale Operation des Timer1
+    TIMSK0 = (1 << OCIE0A) | (1 << TOIE0); // Timer für die Tonausgabe, ISR zum debuggen
+    TIMSK2 = (1 << OCIE2A); // Timer für das auslesen der Samples
 
-    OCR1A = 1451; // Vergleichswert festlegen
+    OCR0A = 128;
+    OCR0B = 128;
+    OCR2A = 250; // Samplingrate = 8kHz; Prescaler = 8; F_CPU = 16 MHz
 
-    TCNT1 = 0;
+    TCNT0 = 1;
+    TCNT2 = 1;
 
     lcd_init();
 
@@ -56,59 +119,107 @@ void init(void)
 
 }
 
-int main(void) {
-
-    init();
-
-    do {
-        // Hier das wav ausgeben
-
-        if ( mm_btnchk(PC0) )
-            timerCtrl(1);
-        if ( mm_btnchk(PC1) )
-            timerCtrl(0);
-
-        _delay_ms(100);
-
-        mm_LEDCode(2); // toggle status led
-
-
-    } while (1==1);
-
-    return 0;
-
-}
-
-void timerCtl(unsigned stat)
+void timerCtrl(unsigned stat)
 {
         switch ( stat )
         {
+            case 0 :
+                // Timer stoppen
+                TCCR0A = 0x00;
+                TCCR0B = 0x00;
+                break;
             case 1:
                 // Timer starten
-                TCNT1 = 1;
-                TCCR1A = 0x00;
-                TCCR1A |= (1 << WGM11); // CTC enabled
-                TCCR1B |= (1 << CS10); // Prescaler = CPU-Takt
-            break;
+                TCCR0A = 0x00;
+                TCCR0A |= (1<<COM0A1) | (1<<COM0B1) | (1<<WGM00) | (1<<WGM01); // Fast PWM, nnicht invertierter Output an OC1A und OC1B
+                TCCR0B |= (1<<CS00); // Prescaler = 1
+                OCR0A = 128;
+                OCR0B = 128;
+                break;
             case 2: // Toggle des Timers
-                if ( TCCR1B & (1 << CS10) )
-                    timerCtl(0); // abschalten
+                if ( TCCR0B & (1 << CS00) )
+                    timerCtrl(0); // abschalten
                 else
-                    timerCtl(1); // anschalten
-            break;
-            default :
+                    timerCtrl(1); // anschalten
+                break;
+            case 3 :
                 // Timer stoppen
-                TCCR1A = 0x00;
-                TCCR1B = 0x00;
+                TCCR2A = 0x00;
+                TCCR2B = 0x00;
+                break;
+            case 4:
+                // Timer starten
+                TCCR2A = 0x00;
+                TCCR2A |= (1<<WGM21); // CTC
+                TCCR2B |= (1<<CS21); // Prescaler = 8
+                break;
+            case 5: // Toggle des Timers
+                if ( TCCR2B & (1 << CS21) )
+                    timerCtrl(3); // abschalten
+                else
+                    timerCtrl(4); // anschalten
+                break;
+            default:
+                timerCtrl(0);
+                timerCtrl(3);
         }
 }
 
 
-ISR (TIMER1_COMPA_vect)
-{
-  // Hier das neue Sample laden und die Ausgabe einleiten
 
-  // ?berlauf des Timers (TIMER1)
-  //PORTD ^= (1 << PD5 ); // Toggeln des Summer Pin's.
+ISR (TIMER0_OVF_vect)
+{
+//    mm_BeepCode(0); // Summer Pin ein
+    mm_LEDCode(1); // toggle status led
+}
+
+ISR (TIMER0_COMPA_vect)
+{
+    // ueberlauf des Timers (TIMER0)
+//    mm_BeepCode(1);
+    mm_LEDCode(0);
+}
+
+/*
+ISR (TIMER0_COMPB_vect)
+{
+    // ueberlauf des Timers (TIMER0)
+    //mm_BeepCode(2);
+    //mm_LEDCode(2);
+}
+*/
+
+ISR (TIMER2_COMPA_vect)
+{
+    // Hier das neue Sample laden und die Ausgabe einleiten
+    wavepos++;
+
+    if (wavepos >= data_wav_len) {
+
+        wavepos = 0; // fuer die Endlosschleife und den neustart
+        timerCtrl(0);
+        timerCtrl(3);
+
+        lcd_gotoxy(0,0);
+        lcd_putstr(soff);
+        
+        lcd_gotoxy(0,0);
+        lcd_putstr(soff);
+
+    } else {
+
+        uint8_t val = pgm_read_byte(&data_wav[wavepos]);
+        OCR0A = val; // Neuen Vergleichswert festlegen
+        OCR0B = val;
+        
+        lcd_gotoxy(0,1);
+        char sval[10];
+        utoa( val, sval, 10 );
+        lcd_putstr(sval);
+
+    }
+
+    //mm_LEDCode(2);
+    
 }
 
--- firmware/main.h
+++ firmware/main.h
@@ -1,9 +1,20 @@
 
 #define F_CPU 16000000UL
 
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdlib.h>
+#include <util/delay.h>
+#include <avr/pgmspace.h>
+
 #include "lcd_lib.h"
 #include "minimexle.h"
 
+volatile uint16_t wavepos = 0;
+
+char* son  = "on ";
+char* soff = "off";
+
 void init(void);
-void timerCtl(unsigned);
+void timerCtrl(unsigned);
 
